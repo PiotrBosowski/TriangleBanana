4b76eef91171308eba1a6c88e41ffb1c1c4f5795
piotr bosowski <piotr.bosowski@gmail.com>
2020-03-16 20:13:46 +0100

    Finish first task

---
+++ ThirdTask.py
@@ -0,0 +1,34 @@
+def check_digit_groups(string):
+    is_in_group = False
+    number_of_groups = 0
+    for i in range(1, len(string)):
+        if string[i] == string[i-1]:
+            if not is_in_group:
+                is_in_group = True
+                number_of_groups += 1
+        else:
+            is_in_group = False
+    return number_of_groups >= 2
+
+
+def check_digits_increasing(string):
+    for i in range(1, len(string)):
+        if string[i-1] > string[i]:
+            return False
+    return True
+
+
+lowerCap = 372 ** 2
+upperCap = 809 ** 2
+counter = 0
+for number in range(lowerCap, upperCap + 1):
+    number_txt = str(number)
+    if check_digits_increasing(number_txt):
+        if check_digit_groups(number_txt):
+            print(number_txt)
+            counter += 1
+print("number of matching passwords: " + str(counter))
+
+
+
+
